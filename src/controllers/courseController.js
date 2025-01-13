c7b89a83b994687ab946845d82fa84d72c36c86d
hamzaelghazouani1 <hamzaelghazouani1>
2025-01-13 02:27:40 +0100

    feat: implement `CRUD functions` for courses in `courseController.js`

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -14,7 +14,55 @@ async function createCourse(req, res) {
     res.status(201).json(course);
 }
 
+async function createCourses(req, res) {
+    const course = await mongoService.insertMany('courses', req.body);
+    res.status(201).json(course);
+}
+
+async function updateCourse(req, res) {
+    const course = await mongoService.updateOne('courses', req.body);
+    res.status(200).json(course);
+}
+
+async function updateCourses(req, res) {
+    const course = await mongoService.updateMany('courses', req.body);
+    res.status(200).json(course);
+}
+
+async function deleteCourse(req, res) {
+    const course = await mongoService.deleteOneById('courses', req.params.id);
+    res.status(200).json(course);
+}
+
+async function deleteCourses(req, res) {
+    const course = await mongoService.deleteMany('courses', req.body);
+    res.status(200).json(course);
+}
+
+async function getCourse(req, res) {
+    const course = await mongoService.findOneById('courses', req.params.id);
+    res.status(200).json(course);
+}
+
+async function getCourses(req, res) {
+    const courses = await mongoService.find('courses',{});
+    res.status(200).json(courses);
+}
+
+async function getCourseByStats(req, res) {
+    const courses = await mongoService.find('courses',{ stats: { $eq: req.body.stats} });
+    res.status(200).json(courses);
+}
+
 
 module.exports = {
   createCourse,
+  createCourses,
+  updateCourse,
+  updateCourses,
+  deleteCourse,
+  deleteCourses,
+  getCourses,
+  getCourse,
+  getCourseByStats
 };
